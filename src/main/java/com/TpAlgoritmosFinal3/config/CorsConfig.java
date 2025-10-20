0c9f3c1e2fc03d36b7258159d827b57225a0e6e9
Tomi-Gomez <tomasngomez1@gmail.com>
2025-10-19 23:56:53 -0300

    agregado url

--- src/main/java/com/TpAlgoritmosFinal3/config/CorsConfig.java
+++ src/main/java/com/TpAlgoritmosFinal3/config/CorsConfig.java
@@ -8,7 +8,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("*")
+                .allowedOrigins("https://front-algoritmos3.vercel.app")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedMethods("*")
                 .allowCredentials(true);
